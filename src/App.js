a35f8de6c2e66fd74638174719d97735cd4c7aae
chris <rckoenig@gmx.de>
2021-06-02 17:44:45 +0200

    Change Animation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import React from 'react';
-// import Hiro from "./components/hiro/Hiro";
+import Hiro from "./components/hiro/Hiro";
 import Earth from "./components/earth/Earth";
 
 function App() {
   return (
     <div className="App">
-      {/* <Hiro/> */}
+      {/* <Earth/> */}
 
-     < Earth/>
+     < Hiro/>
     </div>
   );
 }
