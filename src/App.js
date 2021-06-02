ff82291cb56e7dbbaa2b6eca996a6d22779b2c79
chris <rckoenig@gmx.de>
2021-06-02 17:51:38 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 
 import React from 'react';
-import Hiro from "./components/hiro/Hiro";
 import Earth from "./components/earth/Earth";
 
 function App() {
   return (
     <div className="App">
-      {/* <Earth/> */}
+      <Earth/> 
 
-     < Hiro/>
     </div>
   );
 }
