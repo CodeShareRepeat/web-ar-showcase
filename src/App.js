1179497f96107b3639d4f836568f221717388183
chris <rckoenig@gmx.de>
2021-06-02 17:26:26 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React from 'react';
 // import Hiro from "./components/hiro/Hiro";
 import Earth from "./components/earth/Earth";
 import TrainTicket from "./components/trainTicket/TrainTicket";
-
+import Hiro from "./components/hiro/Hiro";
 function App() {
   return (
     <div className="App">
       {/* <Hiro/> */}
 
-     //< Earth/>
+     <Hiro/>
      <TrainTicket/>
     </div>
   );
